c9de3e4390c1e7cf07714cdb82850efbacef1c7a
unknown <aswalshrey@gmail.com>
2022-12-22 16:23:50 +0530

    React PROPS

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
 import './App.css';
-import elephant from "./images/elephant.jpeg";
-
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
+// import elephant from "./images/elephant.jpeg";
+// import imageData from './components/DataComponents';
 
 
-function App() {
+
+
+function App({shrey}) {
   // code here
-  
+  console.log(shrey)
   return (
     <div className='Container'>
       <h1>Kalvium Gallery</h1>
       <div className='pictureContainer'>
-      {imageData().map((e) => {
 
-        return <img className='pictures' src={e.img} alt='elephant'></img>
-
-      })}
+        {shrey.map((pic) => {
+          return(
+          <img className='pictures' src={pic.img} alt='elephant'></img>
+          )
+          
+        })}
       </div>
-      
     </div>
-
   )
 }
 
--- src/AppClass.js
+++ src/AppClass.js
@@ -1,30 +1,12 @@
 import { Component } from "react";
 import "./App.css"
-import elephant from "./images/elephant.jpeg";
+// import data from './components/DataComponents';
+
+
+// import elephant from "./images/elephant.jpeg";
 
 export default class AppClass extends Component{
   
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
 
   // code here
   render(){
@@ -33,7 +15,7 @@ export default class AppClass extends Component{
     <div className='Container'>
       <h1>Kalvium Gallery</h1>
       <div className='pictureContainer'>
-      {this.imageData().map((e) => {
+      {this.props.shrey.map((e) => {
 
         return <img className='pictures' src={e.img} alt='elephant'></img>
 
